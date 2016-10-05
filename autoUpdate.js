12649d600fbbd8ae3678108c767ad40608ea4535
balsoft <balsoft@yandex.ru>
2016-10-05 19:31:56 +0300

    animations!

--- autoUpdate.js
+++ autoUpdate.js
@@ -1,4 +1,18 @@
 setInterval(function() {
     var iframe = $('iframe', parent.document.body);
     iframe.height($(document.body).height());
-},50);
+},5);
+var animating = false;
+
+			$("img").hover(function () {
+				if (!animating) {
+				animating=true;
+				$(this).animate({width: "50%"},500,"swing",function () {animating=false;});
+			}
+			},
+			function () {
+				if (!animating) {
+				animating=true;
+				$(this).animate({width: "100px"},500,"swing",function () {animating=false;});
+			}
+			});
