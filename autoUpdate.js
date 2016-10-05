42dd3239658a210d07228a43f1c8ed333c0c4f5b
balsoft <balsoft@yandex.ru>
2016-10-05 21:46:36 +0300

    ANIMATIONSgit add .!

--- autoUpdate.js
+++ autoUpdate.js
@@ -1,18 +1,17 @@
 setInterval(function() {
     var iframe = $('iframe', parent.document.body);
     iframe.height($(document.body).height());
-},5);
-var animating = false;
+},2);
 
 			$("img").hover(function () {
-				if (!animating) {
-				animating=true;
-				$(this).animate({width: "50%"},500,"swing",function () {animating=false;});
+				if (!this.animatingBig) {
+				this.animatingBig=true;
+				$(this).animate({width: "50%"},500,"swing",function () {this.animatingBig=false;});
 			}
 			},
 			function () {
-				if (!animating) {
-				animating=true;
-				$(this).animate({width: "100px"},500,"swing",function () {animating=false;});
+				if (!this.animatingSmall) {
+				this.animatingSmall=true;
+				$(this).animate({width: "100px"},500,"swing",function () {this.animatingSmall=false;});
 			}
 			});
