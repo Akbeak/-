bd4f0f039f61aec00bb1da67f1c17f42ea1444da
balsoft <balsoft@yandex.ru>
2016-10-06 18:49:32 +0300

    adjustments

--- autoUpdate.js
+++ autoUpdate.js
@@ -3,15 +3,16 @@ setInterval(function() {
     iframe.height($(document.body).height());
 },2);
 
-			$("img").hover(function () {
+			$("img").click(function () {
+				if (!this.big) {
 				if (!this.animatingBig) {
 				this.animatingBig=true;
-				$(this).animate({width: "50%"},500,"swing",function () {this.animatingBig=false;});
-			}
-			},
-			function () {
+				$(this).animate({width: "50%"},500,"swing",function () {this.animatingBig=false; this.big = true;});
+			}}else{
 				if (!this.animatingSmall) {
 				this.animatingSmall=true;
-				$(this).animate({width: "10%"},500,"swing",function () {this.animatingSmall=false;});
+				$(this).animate({width: "10%"},500,"swing",function () {this.animatingSmall=false; this.big = false;});
 			}
-			});
+			}
+		
+	});
